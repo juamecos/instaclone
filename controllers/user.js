8a44b820fe60ac074b03141884baf9d6ecd625d9
juamecos <juamecos@gmail.com>
2020-09-04 16:45:23 +0200

    user with auth

---
+++ controllers/user.js
@@ -0,0 +1,76 @@
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+// Funcion para generar token
+function createToken(user, SECRET_KEY, expiresIn) {
+  const { id, name, email, username } = user;
+  const payload = {
+    id,
+    name,
+    email,
+    username,
+  };
+  return jwt.sign(payload, SECRET_KEY, { expiresIn });
+}
+
+async function register(input) {
+  const newUser = input;
+
+  // Formatear email y username
+  newUser.email = newUser.email.toLowerCase();
+  newUser.username = newUser.username.toLowerCase();
+
+  const { email, username, password } = newUser;
+
+  // revisar si el email está en uso
+  const foundEmail = await User.findOne({ email });
+  if (foundEmail) throw new Error("El email ya está en uso");
+
+  // revisar si el username está en uso
+  const foundUsername = await User.findOne({ username });
+  if (foundUsername) throw new Error("El nombre de usuario ya existe");
+
+  // Encriptar contraseña
+  const salt = await bcrypt.genSaltSync(10);
+  newUser.password = await bcrypt.hash(password, salt);
+
+  try {
+    const user = new User(newUser);
+    user.save();
+    return user;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function login(input) {
+  const { email, password } = input;
+
+  // Revisar si el email existe en la base de datos
+  const userFound = await User.findOne({ email: email.toLowerCase() });
+  if (!userFound) throw new Error("Error en el email o contraseña");
+
+  // Revisar si la contraseña es correcta
+  const passwordSuccess = await bcrypt.compare(password, userFound.password);
+  if (!passwordSuccess) throw new Error("Error en el email o contraseña");
+
+  // Crear token
+  return {
+    token: createToken(userFound, process.env.SECRET_KEY, "24h"),
+  };
+}
+
+async function getUser(id, username) {
+  let user = null;
+  if (id) user = await User.findById(id);
+  if (username) user = await User.findOne({ username });
+  if (!user) throw new Error("El usuerio no existe");
+  return user;
+}
+
+module.exports = {
+  register,
+  login,
+  getUser,
+};
---
+++ gql/schema.js
@@ -0,0 +1,43 @@
+const { gql } = require("apollo-server");
+
+const typeDefs = gql`
+  type User {
+    id: ID
+    name: String
+    username: String
+    email: String
+    siteWeb: String
+    description: String
+    password: String
+    avatar: String
+    createAt: String
+  }
+
+  type Token {
+    token: String
+  }
+
+  input UserInput {
+    name: String!
+    username: String!
+    email: String!
+    password: String!
+  }
+
+  input loginInput {
+    email: String!
+    password: String!
+  }
+
+  type Query {
+    # User
+    getUser(id: ID, username: String): User
+  }
+
+  type Mutation {
+    # User register
+    register(input: UserInput): User
+    login(input: loginInput): Token
+  }
+`;
+module.exports = typeDefs;
